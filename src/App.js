447cf4c1e91e28cf53594bceafbe9ecc904a4493
Vitaly <blackjack358@gmail.com>
2020-07-11 22:46:14 +0300

    Fix app address.

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("https://face-recognition-app11111.herokuapp.com:3000/image", {
+          fetch("https://face-recognition-app11111.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
